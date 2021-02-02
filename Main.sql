116b72bedb1620bd61178e5135a22ac8ccc2854e
Katherine Seng <43624009+KatherineSeng@users.noreply.github.com>
2021-02-02 09:51:22 -0800

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -30,4 +30,26 @@ Favorites BOOL,
 CONSTRAINT parkingspot_pk PRIMARY KEY (Latitude, Longitude)
 );
 
+-- Table for User Feedback page
+CREATE TABLE ISSUE (
+issue_id INT(10) NOT NULL,
+issue_title VARCHAR(30) NOT NULL,
+issue_description VARCHAR(300) NOT NULL,
+issue_picture BLOB,
+Username VARCHAR(30) NOT NULL,
 
+CONSTRAINT issue_pk PRIMARY KEY (issue_ID),
+CONSTRAINT issue_fk FOREIGN KEY (Username) REFERENCES ACCOUNT (Username)
+);
+
+-- Table for Community Engagement page
+CREATE TABLE DISCUSSION (
+discussion_id INT(10) NOT NULL,
+discussion_title VARCHAR(30) NOT NULL,
+discussion_description VARCHAR(300) NOT NULL,
+discussion_picture BLOB,
+Username VARCHAR(30) NOT NULL,
+
+CONSTRAINT discussion_pk PRIMARY KEY (discussion_ID),
+CONSTRAINT discussion_fk FOREIGN KEY (Username) REFERENCES ACCOUNT (Username)
+);
