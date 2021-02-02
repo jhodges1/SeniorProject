92c4bb924e7404d1e047a9da4d6a731c44053674
Katherine Seng <43624009+KatherineSeng@users.noreply.github.com>
2021-02-02 10:11:21 -0800

    Update Main.sql

--- Main.sql
+++ Main.sql
@@ -1,55 +1,65 @@
+-- Table for a User Account
 CREATE TABLE ACCOUNT (
-FirstName VARCHAR(30) NOT NULL,
-LastName VARCHAR(30) NOT NULL,
-Email VARCHAR(30) NOT NULL,
-Username VARCHAR(30) NOT NULL,
-Password VARCHAR(30) NOT NULL,
+account_id INT NOT NULL,
+account_firstname VARCHAR(30) NOT NULL,
+account_lastname VARCHAR(30) NOT NULL,
+account_username VARCHAR(30) NOT NULL,
+account_password VARCHAR(30) NOT NULL,
+account_email VARCHAR(30) NOT NULL,
 
-CONSTRAINT account_pk PRIMARY KEY (Email),
-CONSTRAINT account_uk UNIQUE KEY (Username)
-);
-
-CREATE TABLE SETTINGS (
-TextMessageReminders BOOL,
-PushNotifications BOOL,
-VoiceShortcuts BOOL,
-EmailValidation BOOL,
-Email VARCHAR(30) NOT NULL,
-
-CONSTRAINT settings_fk FOREIGN KEY (Email) REFERENCES ACCOUNT (Email)
-);
-
-CREATE TABLE PARKINGSPOT (
-StreetAddress VARCHAR(30) NOT NULL,
-Latitude VARCHAR(30) NOT NULL,
-Longitude VARCHAR(30) NOT NULL,
-DaysAvailable VARCHAR(30) NOT NULL,
-TimesAvailable VARCHAR(30) NOT NULL,
-Favorites BOOL,
-
-CONSTRAINT parkingspot_pk PRIMARY KEY (Latitude, Longitude)
-);
-
--- Table for User Feedback page
-CREATE TABLE ISSUE (
-issue_id INT(10) NOT NULL,
-issue_title VARCHAR(30) NOT NULL,
-issue_description VARCHAR(300) NOT NULL,
-issue_picture BLOB,
-Username VARCHAR(30) NOT NULL,
-
-CONSTRAINT issue_pk PRIMARY KEY (issue_ID),
-CONSTRAINT issue_fk FOREIGN KEY (Username) REFERENCES ACCOUNT (Username)
+CONSTRAINT account_pk PRIMARY KEY (account_id),
+CONSTRAINT account_uk01 UNIQUE KEY (account_username),
+CONSTRAINT account_uk02 UNIQUE KEY (account_email)
 );
 
 -- Table for Community Engagement page
 CREATE TABLE DISCUSSION (
-discussion_id INT(10) NOT NULL,
+discussion_id INT NOT NULL,
 discussion_title VARCHAR(30) NOT NULL,
 discussion_description VARCHAR(300) NOT NULL,
 discussion_picture BLOB,
-Username VARCHAR(30) NOT NULL,
+account_id INT NOT NULL,
+account_username VARCHAR(30) NOT NULL,
 
 CONSTRAINT discussion_pk PRIMARY KEY (discussion_ID),
-CONSTRAINT discussion_fk FOREIGN KEY (Username) REFERENCES ACCOUNT (Username)
+CONSTRAINT discussion_fk01 FOREIGN KEY (account_id) REFERENCES ACCOUNT (account_id),
+CONSTRAINT discussion_fk02 FOREIGN KEY (account_username) REFERENCES ACCOUNT (account_username)
 );
+
+-- Table for User Feedback page
+CREATE TABLE ISSUE (
+issue_id INT NOT NULL,
+issue_title VARCHAR(30) NOT NULL,
+issue_description VARCHAR(300) NOT NULL,
+issue_picture BLOB,
+account_id INT NOT NULL,
+account_username VARCHAR(30) NOT NULL,
+
+CONSTRAINT issue_pk PRIMARY KEY (issue_ID),
+CONSTRAINT issue_fk01 FOREIGN KEY (account_id) REFERENCES ACCOUNT (account_id),
+CONSTRAINT issue_fk02 FOREIGN KEY (account_username) REFERENCES ACCOUNT (account_username)
+);
+
+
+
+
+-- CREATE TABLE SETTINGS (
+-- TextMessageReminders BOOL,
+-- PushNotifications BOOL,
+-- VoiceShortcuts BOOL,
+-- EmailValidation BOOL,
+-- Email VARCHAR(30) NOT NULL,
+
+-- CONSTRAINT settings_fk FOREIGN KEY (Email) REFERENCES ACCOUNT (Email)
+-- );
+
+-- CREATE TABLE PARKINGSPOT (
+-- StreetAddress VARCHAR(30) NOT NULL,
+-- Latitude VARCHAR(30) NOT NULL,
+-- Longitude VARCHAR(30) NOT NULL,
+-- DaysAvailable VARCHAR(30) NOT NULL,
+-- TimesAvailable VARCHAR(30) NOT NULL,
+-- Favorites BOOL,
+
+-- CONSTRAINT parkingspot_pk PRIMARY KEY (Latitude, Longitude)
+-- );
