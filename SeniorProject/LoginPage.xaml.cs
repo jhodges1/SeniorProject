5885c199f96bb7d48917c980472bb07cf6200fa3
Katherine Seng <43624009+KatherineSeng@users.noreply.github.com>
2021-02-09 10:11:07 -0800

    Update LoginPage.xaml.cs

--- SeniorProject/LoginPage.xaml.cs
+++ SeniorProject/LoginPage.xaml.cs
@@ -1,24 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System;
+using System.Data;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using MySql.Data.MySqlClient;
 
 namespace SeniorProject
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class LoginPage : ContentPage
     {
         public LoginPage()
         {
             InitializeComponent();
 
-            // Gets rid of extra blue navigation bar that occurs
+            // Gets rid of the extra blue navigation bar that occurs
             NavigationPage.SetHasNavigationBar(this, false);
 
+        }
+
+        private void btnRegisterAccount_Click()
+        {
+            // redirect to register account page
+        }
+
+        private void btnLogin_Click()
+        {
+            try
+            {
+                // Notes:
+                // UID & PASSWORD mapped to XAML buttons
+                string connectionString = "SERVER=localhost;DATABASE=TeamBeach_Database;UID=root;PASSWORD=root;";
+
+                MySqlConnection connection = new MySqlConnection(connectionString);
+
+                MySqlCommand cmd = new MySqlCommand("select * from account", connection);
+
+                DataTable dt = new DataTable();
+
+                connection.Open();
+                dt.Load(cmd.ExecuteReader());
+                connection.Close();
+
+                // If cmd successful, redirect to Home Page
+            }
+            catch (Exception)
+            { 
+                // If cmd failed, retry Login Page
+            }
         }
 
         private async void NavigateButton_OnClicked(object sender, EventArgs e)
