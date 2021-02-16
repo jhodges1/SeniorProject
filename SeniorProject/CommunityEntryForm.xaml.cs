aa90f160bcb37dc7cdc2f41bc45dd8b8db28c7e8
AngelaDo45 <angela.do45@gmail.com>
2021-02-15 20:56:32 -0800

    Adds community Entry Form Page

---
+++ SeniorProject/CommunityEntryForm.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SeniorProject
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CommunityEntryForm : ContentPage
+    {
+        private Entry titleEntry;
+        private Entry descriptionEntry;
+        private Button submitButton;
+
+        string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "myDB.db3");
+        public CommunityEntryForm()
+        {
+            Title = "Community Entry Form";
+
+            StackLayout stackLayout = new StackLayout();
+
+            titleEntry = new Entry();
+            titleEntry.Keyboard = Keyboard.Text;
+            titleEntry.Placeholder = "Enter Title";
+            stackLayout.Children.Add(titleEntry);
+
+            descriptionEntry = new Entry();
+            descriptionEntry.Keyboard = Keyboard.Text;
+            descriptionEntry.Placeholder = "Enter Description";
+            stackLayout.Children.Add(descriptionEntry);
+
+            submitButton = new Button();
+            submitButton.Text = "Submit";
+            submitButton.Clicked += submitButton_clicked;
+            stackLayout.Children.Add(submitButton);
+
+            Content = stackLayout;
+
+            //InitializeComponent();
+
+        }
+        private async void submitButton_clicked(object sender, EventArgs e)
+        {
+            var db = new SQLiteConnection(dbPath);
+            db.CreateTable<CommunityEntry>();
+            var maxPk = db.Table<CommunityEntry>().OrderByDescending(c => c.Id).FirstOrDefault();
+            CommunityEntry communityEntry = new CommunityEntry()
+            {
+                Id = (maxPk == null ? 1 : maxPk.Id + 1),
+                Title = titleEntry.Text,
+                Description = descriptionEntry.Text
+            };
+            db.Insert(communityEntry); 
+            await Navigation.PushAsync(new SubmissionConfirmationPage());
+        }
+
+    }
+}
