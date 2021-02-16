031cebb10d0780b3583507d4b9f25265247d4921
AngelaDo45 <angela.do45@gmail.com>
2021-02-15 20:41:48 -0800

    Create Community Entry Class

---
+++ SeniorProject/CommunityEntry.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeniorProject
+{
+    class CommunityEntry
+    {
+        [PrimaryKey]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return this.Title + "\n" + this.Description;
+        }
+    }
+}
