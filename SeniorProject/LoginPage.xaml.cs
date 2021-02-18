bf22a0fa4d59af82bfb179d5a178a05f598702d1
cristianpardoo <43222699+cristianpardoo@users.noreply.github.com>
2021-02-18 10:21:53 -0800

    Updated for map integration

--- SeniorProject/LoginPage.xaml.cs
+++ SeniorProject/LoginPage.xaml.cs
@@ -7,10 +7,6 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-using MySqlConnector;
-
-using System.Data;
-
 namespace SeniorProject
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -25,60 +21,9 @@ namespace SeniorProject
 
         }
 
-        private async void Login(object sender, EventArgs e)
+        private async void NavigateButton_OnClicked(object sender, EventArgs e)
         {
-            string uid = Username.Text;
-            string pwd = Password.Text;
-
-            //string dbConnection = $"SERVER=localhost;DATABASE=TeamBeach_Database;UID={username};PASSWORD={password};";
-
-            //string dbConnection = "server=localhost;user id=root;database=teambeach_database;persistsecurityinfo=True";
-
-            //string dbConnection = "Server=localhost; Port=3306; Database=teambeach_database; Uid=root; Pwd=root;";
-            
-            MySqlConnectionStringBuilder dbConnection = new MySqlConnectionStringBuilder();
-            dbConnection.Server = "127.0.0.1";
-            dbConnection.Port = 8554;
-            dbConnection.UserID = "root";
-            dbConnection.Password = "root";
-            dbConnection.Database = "teambeach_database";
-
-            MySqlConnection connection = new MySqlConnection(dbConnection.ToString());
-            
-            //MySqlConnection connection = new MySqlConnection(dbConnection);
-            
-            MySqlCommand cmd = new MySqlCommand("select * from account", connection);
-
-            DataTable dt = new DataTable();
-
-            connection.Open(); // Error !
-
-            dt.Load(cmd.ExecuteReader());
-
-            // Login failed
-            if (cmd.ExecuteReader() == null)
-            {
-                await DisplayAlert("Error", "Invalid username or password", "OK");
-            }
-            // Login successful
-            else
-            {
-                await Navigation.PushModalAsync(new MainPage());
-            }
-
-            connection.Close();
-        }
-
-        private async void Login_OnClicked(object sender, EventArgs e)
-        {
-            await DisplayAlert("Alert", "Login successful!", "OK");
-
-            await Navigation.PushModalAsync(new MainPage()); 
-        }
-
-        private async void Register_OnClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new RegisterPage());
+            await Navigation.PushAsync(new MainPage());
         }
     }
 }
